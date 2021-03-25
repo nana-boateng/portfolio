a3b7e4123c62c90474c2bfbb8e7d0c22a4786de0
Nana B <nana.boateng@queensu.ca>
2021-03-25 15:14:42 -0400

    Add react-icons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,12 @@
-export default function Carousel({}) {
-  const skills = ["Git", "Jira", "Bitbucket", "GitHub", "Nodejs"];
+import { FaGit, FaJira } from "react-icons/fa";
+import { GrNode } from "react-icons/gr";
 
-  return <div>{skills.map((skill) => console.log(skill))}</div>;
+export default function Carousel() {
+  return (
+    <div className="flex flex-row justify-evenly w-1/3 mx-auto">
+      <FaGit className={`h-10 w-10`} />
+      <FaJira className={`h-10 w-10`} />
+      <GrNode className={`h-10 w-10`} />
+    </div>
+  );
 }
