e9cf2ee3688cce23a5c0ab6ef6320961346a0fef
Nana <nana.boateng@queensu.ca>
2021-04-04 11:42:20 -0400

    Add custom colours and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,67 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        celeste: "#9BF6FF",
+        orange: "#ffd6a5",
+        pink: "#ffadad",
+        yellow: "#fdffb6",
+        green: "#caffbf",
+        indigo: "#a0c4ff",
+        purple: "#bdb2ff",
+        mauve: "#ffc6ff",
+        powder: "#fffffc",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+      },
+    },
   },
   variants: {
     extend: {},
