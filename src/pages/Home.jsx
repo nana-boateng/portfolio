6004f9b3d7dc0170f50dd5f9321fe6e38cbbcab6
nana <nana.boateng@queensu.ca>
2021-04-05 16:51:32 -0400

    Componentalize

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,55 @@
+import { Link } from "react-router-dom";
+
 // components
+import Nav from "./../components/Nav";
 // import Summary from "../components/Summary";
-import PageHeader from "./../components/PageHeader";
-import Quote from "./../components/Quote";
+import PageHeader from "./../components/home/PageHeader";
+import Quote from "../components/home/Quote";
 // import Carousel from "./../components/Carousel";
 // import Projects from "./../components/Projects";
-import ArrowDown from "./../components/icons/ArrowDown";
-import ContactIcons from "./../components/ContactIcons";
+import { ArrowDown } from "./../components/icons/";
+import NavItem from "../components/NavItem";
+import { Code, Connect, Contact } from "../components/home";
 
 export default function Home() {
   // create projects object with title and description
-  const projects = {
-    ".dotfiles": {
-      title: ".dotfiles",
-      description: "My dotfiles that I take with me everywhere.",
-      stack: ["Lua", "Bash"],
-    },
-    "glass-o-milk": {
-      title: "Glass of Milk",
-      description:
-        "A web app to help you make healthier eating choices by providing healthy meals you can make with items in your fridge.",
-      stack: ["CSS3", "React", "Strapi", "Docker", "Kubernetes"],
-    },
-  };
+  // const projects = {
+  //   ".dotfiles": {
+  //     title: ".dotfiles",
+  //     description: "My dotfiles that I take with me everywhere.",
+  //     stack: ["Lua", "Bash"],
+  //   },
+  //   "glass-o-milk": {
+  //     title: "Glass of Milk",
+  //     description:
+  //       "A web app to help you make healthier eating choices by providing healthy meals you can make with items in your fridge.",
+  //     stack: ["CSS3", "React", "Strapi", "Docker", "Kubernetes"],
+  //   },
+  // };
 
   return (
     <div className="h-screen bg-celeste flex flex-col justify-between">
-      <div>
-        <ContactIcons />
-      </div>
+      <Nav style={`text-xl m-3 h-6`}>
+        <NavItem>
+          <Code />
+        </NavItem>
+        <NavItem>
+          <Contact />
+        </NavItem>
+        <NavItem>
+          <Connect />
+        </NavItem>
+      </Nav>
 
       <div className="mx-auto ">
-        <PageHeader title="nana" />
-        <div className="flex flex-row text-2xl space-between">
-          <div className="animate-fade-in-up mx-3">software developer </div>
-          <div>/</div>
-          <div className="animate-fade-in-up mx-3">computer scientist</div>
-        </div>
+        <PageHeader />
       </div>
 
       <div className="mx-auto my-3">
         <Quote />
-        <ArrowDown />
+        <Link to="/skills">
+          <ArrowDown animation="animate-bounce" />
+        </Link>
       </div>
       {/* Summary */}
       {/* <Summary /> */}
