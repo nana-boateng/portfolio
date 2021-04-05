0ad3df5fa2b51b9bd1324deb3ab5d5b52fd404ce
nana <nana.boateng@queensu.ca>
2021-04-05 16:47:07 -0400

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 // pages
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/skills" component={Skills} />
+      {/* <Home />
+      <Skills /> */}
+    </Router>
+  );
 }
 
 export default App;
